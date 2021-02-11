5ef2e799d2e65769489ba7a2dd9c5bc1f50f6ff6
Antonio Cardace <acardace@redhat.com>
2021-02-11 17:44:04 +0100

    Update KubeVirt to use apiextensions.k8s.io/v1
    Change apiextensions.k8s.io/v1beta1 to apiextensions.k8s.io/v1
    as the former is deprecated and will be removed from Kubernetes 1.22.
    
    Signed-off-by: Antonio Cardace <acardace@redhat.com>

--- pkg/virt-operator/resource/apply/crds.go
+++ pkg/virt-operator/resource/apply/crds.go
@@ -5,12 +5,41 @@ import (
 	"encoding/json"
 	"fmt"
 
-	extv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
 	"kubevirt.io/client-go/log"
 )
+
+func getSubresourcesForVersion(crd *extv1.CustomResourceDefinition, version string) *extv1.CustomResourceSubresources {
+	for _, v := range crd.Spec.Versions {
+		if version == v.Name {
+			return v.Subresources
+		}
+	}
+	return nil
+}
+
+func needsSubresourceStatusEnable(crd, cachedCrd *extv1.CustomResourceDefinition) bool {
+	for _, version := range crd.Spec.Versions {
+		if version.Subresources != nil && version.Subresources.Status != nil {
+			subresource := getSubresourcesForVersion(cachedCrd, version.Name)
+			if subresource == nil || subresource.Status == nil {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func needsSubresourceStatusDisable(crdTargetVersion *extv1.CustomResourceDefinitionVersion, cachedCrd *extv1.CustomResourceDefinition) bool {
+	// subresource support needs to be introduced carefully after the control plane roll-over
+	// to avoid creating zombie entities which don't get processed due to ignored status updates
+	subresource := getSubresourcesForVersion(cachedCrd, crdTargetVersion.Name)
+	return (subresource == nil || subresource.Status == nil) &&
+		(crdTargetVersion.Subresources != nil && crdTargetVersion.Subresources.Status != nil)
+}
 
 func (r *Reconciler) createOrUpdateCrds() error {
 	for _, crd := range r.targetStrategy.CRDs() {
@@ -23,22 +52,22 @@ func (r *Reconciler) createOrUpdateCrds() error {
 	return nil
 }
 
-func (r *Reconciler) createOrUpdateCrd(crd *extv1beta1.CustomResourceDefinition) error {
-	ext := r.clientset.ExtensionsClient()
+func (r *Reconciler) createOrUpdateCrd(crd *extv1.CustomResourceDefinition) error {
+	client := r.clientset.ExtensionsClient()
 	version, imageRegistry, id := getTargetVersionRegistryID(r.kv)
-	var cachedCrd *extv1beta1.CustomResourceDefinition
+	var cachedCrd *extv1.CustomResourceDefinition
 
 	crd = crd.DeepCopy()
 	obj, exists, _ := r.stores.CrdCache.Get(crd)
 	if exists {
-		cachedCrd = obj.(*extv1beta1.CustomResourceDefinition)
+		cachedCrd = obj.(*extv1.CustomResourceDefinition)
 	}
 
 	injectOperatorMetadata(r.kv, &crd.ObjectMeta, version, imageRegistry, id, true)
 	if !exists {
 		// Create non existent
 		r.expectations.Crd.RaiseExpectations(r.kvKey, 1, 0)
-		_, err := ext.ApiextensionsV1beta1().CustomResourceDefinitions().Create(context.Background(), crd, metav1.CreateOptions{})
+		_, err := client.ApiextensionsV1().CustomResourceDefinitions().Create(context.Background(), crd, metav1.CreateOptions{})
 		if err != nil {
 			r.expectations.Crd.LowerExpectations(r.kvKey, 1, 0)
 			return fmt.Errorf("unable to create crd %+v: %v", crd, err)
@@ -58,11 +87,9 @@ func (r *Reconciler) createOrUpdateCrd(crd *extv1beta1.CustomResourceDefinition)
 		}
 		ops = append(ops, labelAnnotationPatch...)
 
-		// subresource support needs to be introduced carefully after the control plane roll-over
-		// to avoid creating zombie entities which don't get processed du to ignored status updates
-		if cachedCrd.Spec.Subresources == nil || cachedCrd.Spec.Subresources.Status == nil {
-			if crd.Spec.Subresources != nil && crd.Spec.Subresources.Status != nil {
-				crd.Spec.Subresources.Status = nil
+		for i := range crd.Spec.Versions {
+			if needsSubresourceStatusDisable(&crd.Spec.Versions[i], cachedCrd) {
+				crd.Spec.Versions[i].Subresources.Status = nil
 			}
 		}
 
@@ -73,7 +100,7 @@ func (r *Reconciler) createOrUpdateCrd(crd *extv1beta1.CustomResourceDefinition)
 		}
 		ops = append(ops, fmt.Sprintf(`{ "op": "replace", "path": "/spec", "value": %s }`, string(newSpec)))
 
-		_, err = ext.ApiextensionsV1beta1().CustomResourceDefinitions().Patch(context.Background(), crd.Name, types.JSONPatchType, generatePatchBytes(ops), metav1.PatchOptions{})
+		_, err = client.ApiextensionsV1().CustomResourceDefinitions().Patch(context.Background(), crd.Name, types.JSONPatchType, generatePatchBytes(ops), metav1.PatchOptions{})
 		if err != nil {
 			return fmt.Errorf("unable to patch crd %+v: %v", crd, err)
 		}
@@ -97,16 +124,15 @@ func (r *Reconciler) rolloutNonCompatibleCRDChanges() error {
 	return nil
 }
 
-func (r *Reconciler) rolloutNonCompatibleCRDChange(crd *extv1beta1.CustomResourceDefinition) error {
-
-	ext := r.clientset.ExtensionsClient()
+func (r *Reconciler) rolloutNonCompatibleCRDChange(crd *extv1.CustomResourceDefinition) error {
+	client := r.clientset.ExtensionsClient()
 	version, imageRegistry, id := getTargetVersionRegistryID(r.kv)
-	var cachedCrd *extv1beta1.CustomResourceDefinition
+	var cachedCrd *extv1.CustomResourceDefinition
 
 	crd = crd.DeepCopy()
 	obj, exists, _ := r.stores.CrdCache.Get(crd)
 	if exists {
-		cachedCrd = obj.(*extv1beta1.CustomResourceDefinition)
+		cachedCrd = obj.(*extv1.CustomResourceDefinition)
 	}
 
 	injectOperatorMetadata(r.kv, &crd.ObjectMeta, version, imageRegistry, id, true)
@@ -114,17 +140,11 @@ func (r *Reconciler) rolloutNonCompatibleCRDChange(crd *extv1beta1.CustomResourc
 		// Patch if in the deployed version the subresource is not enabled
 		var ops []string
 
-		// enable the status subresources now, in case that they were disabled before
-		if crd.Spec.Subresources == nil || crd.Spec.Subresources.Status == nil {
+		if !needsSubresourceStatusEnable(crd, cachedCrd) {
 			return nil
 		}
 
-		if crd.Spec.Subresources != nil && crd.Spec.Subresources.Status != nil {
-			if cachedCrd.Spec.Subresources != nil && cachedCrd.Spec.Subresources.Status != nil {
-				return nil
-			}
-		}
-
+		// enable the status subresources now, in case that they were disabled before
 		// Add Spec Patch
 		newSpec, err := json.Marshal(crd.Spec)
 		if err != nil {
@@ -132,7 +152,7 @@ func (r *Reconciler) rolloutNonCompatibleCRDChange(crd *extv1beta1.CustomResourc
 		}
 		ops = append(ops, fmt.Sprintf(`{ "op": "replace", "path": "/spec", "value": %s }`, string(newSpec)))
 
-		_, err = ext.ApiextensionsV1beta1().CustomResourceDefinitions().Patch(context.Background(), crd.Name, types.JSONPatchType, generatePatchBytes(ops), metav1.PatchOptions{})
+		_, err = client.ApiextensionsV1().CustomResourceDefinitions().Patch(context.Background(), crd.Name, types.JSONPatchType, generatePatchBytes(ops), metav1.PatchOptions{})
 		if err != nil {
 			return fmt.Errorf("unable to patch crd %+v: %v", crd, err)
 		}
